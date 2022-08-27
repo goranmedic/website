c5800481397833067cadbb87e95b4ab1e8bf230e
Goran Medic <gmedic@neogov.net>
2022-08-27 15:42:24 +0200

    initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-scroll';
+import PortfolioContext from '../../context/context';
+
+
+const Footer = () => {
+  const { footer } = useContext(PortfolioContext);
+  const { networks } = footer;
+
+  return (
+    <footer className="footer navbar-static-bottom">
+      <Container>
+        <span className="back-to-top">
+          <Link to="hero" smooth duration={1000}>
+            <i className="fa fa-angle-up fa-2x" aria-hidden="true" />
+          </Link>
+        </span>
+        <div>
+        NullReferenceException, obrt za računalno programiranje, vl. GORAN MEDIĆ,
+Split, D. Šimunovića 13
+        </div>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
